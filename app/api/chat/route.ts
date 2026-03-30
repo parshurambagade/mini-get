d8eeafcd3b38d6344e39f3c33d551478706c59f0
parshurambagade <parshurambagade855@gmail.com>
2026-03-30 10:37:27 +0530

    add: basic rag setup with document indexing plus retrival processes

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,12 +2,41 @@ import Groq from "groq-sdk";
 import { tavily } from "@tavily/core";
 import { ChatCompletionMessageParam } from "groq-sdk/resources/chat.mjs";
 import { NextResponse } from "next/server";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import { createClient } from "@supabase/supabase-js";
+import type { Document } from "@langchain/core/documents";
 
 const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
+export const runtime = "nodejs";
+
+const embeddings = new HuggingFaceInferenceEmbeddings({
+  apiKey: process.env.HUGGINGFACEHUB_API_KEY,
+  model: "BAAI/bge-large-en-v1.5",
+  provider: "hf-inference",
+});
+
+const supabaseClient = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_PRIVATE_KEY!,
+);
+
+const vectorStore = new SupabaseVectorStore(embeddings, {
+  client: supabaseClient,
+  tableName: "documents",
+  queryName: "match_documents",
+});
+
 export async function POST(request: Request) {
   const { query, messages } = await request.json();
+
+  // await indexTheDocument(
+  //   "/Users/parshurambagade/Desktop/Developer/projects/gen-ai/mini-gpt/cg-internal-docs.pdf",
+  // );
 
   console.log("Message received by server: ", query);
 
@@ -17,7 +46,19 @@ export async function POST(request: Request) {
       { status: 400 },
     );
 
-  const SYSTEM_PROMPT = `${process.env.SYSTEM_PROMPT} ${new Date().toUTCString()}`;
+  const similaritySearchResults = await vectorStore.similaritySearch(query);
+
+  const context = similaritySearchResults
+    .map((doc) => doc.pageContent)
+    .join("\n\n");
+
+  console.log("Context: ", context);
+
+  // const SYSTEM_PROMPT = `${process.env.SYSTEM_PROMPT} ${new Date().toUTCString()}`;
+
+  const SYSTEM_PROMPT = `You are a smart AI assistent, your task is to answer user queries according to the provided context. If you dont know the answer, be honest and tell the user that you could not find information related to that query. Answer shortly.`;
+
+  const queryWithContext = `CONTEXT: ${context} \n\n QUERY: ${query} \n\n ANSWER:`;
 
   const localMessages: ChatCompletionMessageParam[] = [
     {
@@ -27,7 +68,8 @@ export async function POST(request: Request) {
     ...messages,
     {
       role: "user",
-      content: query,
+      // content: query,
+      content: queryWithContext,
     },
   ];
 
@@ -115,4 +157,43 @@ async function webSearch({ query }: { query: string }) {
     .join("\n\n");
 
   return finalContent || "";
+}
+
+export async function indexTheDocument(docPath: string) {
+  // load the document
+  const loader = new PDFLoader(docPath, {
+    splitPages: false,
+  });
+
+  const singleDoc = await loader.load();
+
+  console.log(singleDoc);
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 100,
+  });
+  const texts = await splitter.splitText(singleDoc[0].pageContent);
+
+  // console.log("Texts: ", texts);
+
+  // agian convert items in texts array in this format {pageContent: string, metadata: {}}
+  const documents: Document[] = [];
+
+  texts.forEach((text) => {
+    documents.push({
+      pageContent: text,
+      metadata: singleDoc[0].metadata,
+    });
+  });
+
+  console.log("docs: ", documents);
+  try {
+    console.log("Supabase client: ", supabaseClient);
+    console.log("Vector Store: ", vectorStore);
+
+    await vectorStore.addDocuments(documents);
+  } catch (error) {
+    console.log("ERROR ADDING DOCS: ", error);
+  }
 }
